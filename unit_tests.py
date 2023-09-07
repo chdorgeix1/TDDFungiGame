5e38809f75e040d387224c9e259bcf16d01d53c5
chdorgeix1 <112966997+chdorgeix1@users.noreply.github.com>
2023-09-07 16:36:02 -0400

    Added tests and functionality for generating world, food, and map

--- unit_tests.py
+++ unit_tests.py
@@ -1,25 +1,121 @@
 import pytest
 from world import World as world
 
-class TestClass:
+class TestWorldClass:
 
     def setup(self):
         self.world = world()
 
     def test_world_dimensions(self):
         assert self.world.dimensions == (0,0)
+        
+    def test_world_sprize_size(self):
+        assert self.world.sprite_size == (0,0)
 
-    def test_set_world_dimension_small(self):
+
+    def test_set_world_dimension_small_dimension(self):
         input = 'small'
         self.world.set_dimensions(input)
         assert self.world.dimensions == (50,50)
 
-    def test_set_world_dimension_medium(self):
+    def test_set_world_dimension_medium_dimension(self):
         input = 'medium'
         self.world.set_dimensions(input)
         assert self.world.dimensions == (100,100)
 
-    def test_set_world_dimension_large(self):
+    def test_set_world_dimension_large_dimension(self):
         input = 'large'
         self.world.set_dimensions(input)
         assert self.world.dimensions == (150,150)
+
+
+    def test_set_world_dimension_small_sprite(self):
+        input = 'small'
+        self.world.set_sprite_size(input)
+        assert self.world.sprite_size == (15,15)
+
+    def test_set_world_dimension_medium_sprite(self):
+        input = 'medium'
+        self.world.set_sprite_size(input)
+        assert self.world.sprite_size == (10,10)
+
+    def test_set_world_dimension_large_sprite(self):
+        input = 'large'
+        self.world.set_sprite_size(input)
+        assert self.world.sprite_size == (5,5)
+
+    def test_generate_world_small(self):
+        input = 'small'
+        self.world.generate_world(input)
+        assert self.world.sprite_size == (15,15)
+        assert self.world.dimensions == (50,50)
+
+    def test_generate_world_medium(self):
+        input = 'medium'
+        self.world.generate_world(input)
+        assert self.world.sprite_size == (10,10)
+        assert self.world.dimensions == (100,100)
+
+    def test_generate_world_large(self):
+        input = 'large'
+        self.world.generate_world(input)
+        assert self.world.sprite_size == (5,5)
+        assert self.world.dimensions == (150,150)
+
+
+class TestSmallWorldClass:
+
+    def setup(self):
+        self.world = world()
+        self.world.generate_world('small')
+
+    def test_generate_random_food(self):
+        self.world.generate_food()
+        assert self.world.food_coords != []
+
+    def test_accurate_food_count(self):
+        self.world.generate_food()
+        assert len(self.world.food_coords) == 50
+
+    def test_generate_map(self):
+        self.world.generate_map()
+        assert self.world.map != None
+    
+
+
+class TestMediumWorldClass:
+
+    def setup(self):
+        self.world = world()
+        self.world.generate_world('medium')
+
+    def test_generate_random_food(self):
+        self.world.generate_food()
+        assert self.world.food_coords != []
+    
+    def test_accurate_food_count(self):
+        self.world.generate_food()
+        assert len(self.world.food_coords) == 100
+
+    def test_generate_map(self):
+        self.world.generate_map()
+        assert self.world.map != None
+
+
+class TestLargeWorldClass:
+
+    def setup(self):
+        self.world = world()
+        self.world.generate_world('large')
+
+    def test_generate_random_food(self):
+        self.world.generate_food()
+        assert self.world.food_coords != []
+
+    def test_accurate_food_count(self):
+        self.world.generate_food()
+        assert len(self.world.food_coords) == 150
+
+    def test_generate_map(self):
+        self.world.generate_map()
+        assert self.world.map != None
--- world.py
+++ world.py
@@ -1,5 +1,5 @@
 import pytest
-#import pygame
+import pygame
 import random
 #from pygame.sprite import Group
 
@@ -7,11 +7,16 @@ import random
     # This impacts dimensions and sprite size
 # Impassable Terrain
 # Food Sources
-
+# A world has a map
+# A world has players
+# A world is fullscreen
 
 class World:
     def __init__(self):
         self.dimensions = (0,0)
+        self.sprite_size = (0,0)
+        self.food_coords = []
+        self.map = None
 
     def set_dimensions(self,input):
         if input == 'small':
@@ -20,3 +25,27 @@ class World:
             self.dimensions = (100,100)
         elif input == 'large':
             self.dimensions = (150,150)  
+    
+    def set_sprite_size(self,input):
+        if input == 'small':
+            self.sprite_size = (15,15)
+        elif input == 'medium':
+            self.sprite_size = (10,10)
+        elif input == 'large':
+            self.sprite_size = (5,5) 
+    
+    def generate_world(self,input):
+        self.set_dimensions(input)
+        self.set_sprite_size(input)
+
+    def generate_food(self):
+        for i in range(0, self.dimensions[0]):
+            x = i
+            y = random.randint(0,self.dimensions[1] - self.sprite_size[1])
+            self.food_coords.append([x,y])
+
+    def generate_map(self):
+        self.map = pygame.display.set_mode(self.dimensions)
+        self.map.fill((50,50,50))
+
+    
